0e89475ae8969a4481ddead450cb142ba489ee32
Ryan Holt <ryan@ryanholt.net>
2025-04-07 18:46:27 -0400

    Update with systemd service to launch serial port on usb

--- modules/services.nix
+++ modules/services.nix
@@ -2,7 +2,9 @@
 {
   services = {
     openssh.enable = true;
-    getty."ttyGS0".enable = true;
     journald.extraConfig = "Storage=volatile";
   };
+
+  # Enable a serial console on ttyGS0 (USB gadget serial)
+  systemd.services."serial-getty@ttyGS0".enable = true;
 }
