5d6508865e463b10b2cda0e7e95a40328c9e25ed
Ryan Holt <ryan@ryanholt.net>
2025-04-07 19:28:14 -0400

    update user config and secret definition

--- modules/users.nix
+++ modules/users.nix
@@ -5,7 +5,7 @@
     isNormalUser = true;
     extraGroups = [ "wheel" ];
     shell = pkgs.fish;
-    passwordFile = config.sops.secrets.ryan_password.path;
+    hashedPasswordFile = config.sops.secrets.ryan_password.path;
     openssh.authorizedKeys.keyFiles = [
       config.sops.secrets.ryan_ssh_public_key.path
     ];
