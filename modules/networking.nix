b17f3865db72460c5861fb36c9c3132993b8e2a5
Ryan Holt <ryan@ryanholt.net>
2025-04-07 18:59:44 -0400

    that was dumb

--- modules/networking.nix
+++ modules/networking.nix
@@ -6,10 +6,10 @@ in {
     wireless.enable = true;
     wireless.networks = {
       "rvproblems-2ghz" = lib.optionalAttrs (wifiSecrets.rvproblems-2ghz != null) {
-        psk = wifiSecrets.RV_WIFI.psk;
+        psk = wifiSecrets.rvproblems-2ghz.psk;
       };
       "iot" = lib.optionalAttrs (wifiSecrets.iot != null) {
-        psk = wifiSecrets.Backup_WIFI.psk;
+        psk = wifiSecrets.iot.psk;
       };
     };
     useDHCP = true;
