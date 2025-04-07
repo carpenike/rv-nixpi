f90018698476e6c2d9e17bd6b6a53af9e7af01c8
Ryan Holt <ryan@ryanholt.net>
2025-04-07 18:57:15 -0400

    define keyfile differently

--- modules/users.nix
+++ modules/users.nix
@@ -6,8 +6,8 @@
     extraGroups = [ "wheel" ];
     shell = pkgs.fish;
     passwordFile = config.sops.secrets.ryan_password.path;
-    openssh.authorizedKeys.keys = [
-      (builtins.readFile config.sops.secrets.ryan_ssh_public_key.path)
+    openssh.authorizedKeys.keyFiles = [
+      config.sops.secrets.ryan_ssh_public_key.path
     ];
   };
 }
