8d6badf880707177f886416388998b134acaaf9a
Ryan Holt <ryan@ryanholt.net>
2025-04-07 19:08:29 -0400

    try pskRaw

--- modules/networking.nix
+++ modules/networking.nix
@@ -5,25 +5,14 @@ let
 in {
   networking = {
     wireless.enable = true;
-
-    # Disable the default network list
-    wireless.networks = {};
-
-    wireless.wpaSupplicantConf = ''
-      ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=wheel
-      update_config=1
-
-      network={
-        ssid="iot"
-        psk="${builtins.readFile wifiSecrets.iot_psk.path}"
-      }
-
-      network={
-        ssid="rvproblems-2ghz"
-        psk="${builtins.readFile wifiSecrets.rvproblems_2ghz_psk.path}"
-      }
-    '';
-
+    wireless.networks = {
+      "iot" = lib.mkIf (wifiSecrets ? iot_psk) {
+        pskRaw = builtins.readFile wifiSecrets.iot_psk.path;
+      };
+      "rvproblems-2ghz" = lib.mkIf (wifiSecrets ? rvproblems_2ghz_psk) {
+        pskRaw = builtins.readFile wifiSecrets.rvproblems_2ghz_psk.path;
+      };
+    };
     useDHCP = true;
     hostName = "nixpi";
   };
