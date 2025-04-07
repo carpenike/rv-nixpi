1d8cba75f5fe3b6789ae30a5b9af3c6f7b01725e
Ryan Holt <ryan@ryanholt.net>
2025-04-07 19:05:21 -0400

    set wifi passwords directly in the wpaSupplicantConf file

--- modules/networking.nix
+++ modules/networking.nix
@@ -1,15 +1,29 @@
 { config, lib, ... }:
-{
+
+let
+  wifiSecrets = config.sops.secrets;
+in {
   networking = {
     wireless.enable = true;
-    wireless.networks = {
-      "iot" = {
-        pskFile = config.sops.secrets.iot_psk.path;
-      };
-      "rvproblems-2ghz" = {
-        pskFile = config.sops.secrets.rvproblems_2ghz_psk.path;
-      };
-    };
+
+    # Disable the default network list
+    wireless.networks = {};
+
+    wireless.wpaSupplicantConf = ''
+      ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=wheel
+      update_config=1
+
+      network={
+        ssid="iot"
+        psk="${builtins.readFile wifiSecrets.iot_psk.path}"
+      }
+
+      network={
+        ssid="rvproblems-2ghz"
+        psk="${builtins.readFile wifiSecrets.rvproblems_2ghz_psk.path}"
+      }
+    '';
+
     useDHCP = true;
     hostName = "nixpi";
   };
