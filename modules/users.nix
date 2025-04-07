948aaf0c1d64f46bd2f305d287a649c8fa9c055c
Ryan Holt <ryan@ryanholt.net>
2025-04-07 18:51:41 -0400

    refrence passwords differently

--- modules/users.nix
+++ modules/users.nix
@@ -1,17 +1,13 @@
 { config, pkgs, lib, ... }:
-let
-  userSecrets = config.sops.secrets or {};
-in {
-  users.users = {
-    ryan = {
-      isNormalUser = true;
-      # Reference the hashed password from the secrets file.
-      password = userSecrets.ryan_password;
-      extraGroups = [ "wheel" ];
-      openssh.authorizedKeys.keys = [
-        userSecrets.ryan_ssh_public_key
-      ];
-      shell = pkgs.fish;
-    };
+
+{
+  users.users.ryan = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    shell = pkgs.fish;
+    passwordFile = config.sops.secrets.ryan_password.path;
+    openssh.authorizedKeys.keys = [
+      (builtins.readFile config.sops.secrets.ryan_ssh_public_key.path)
+    ];
   };
 }
