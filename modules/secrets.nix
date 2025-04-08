53d38b38c198c9790f3a234a5430618de3839435
Ryan Holt <ryan@ryanholt.net>
2025-04-07 20:11:09 -0400

    file is ignored, attempt to inject it

--- modules/secrets.nix
+++ modules/secrets.nix
@@ -1,20 +1,27 @@
 { pkgs, config, lib, ... }:
 
-{
+let
+  bootstrapAgeKey = builtins.readFile ../secrets/age.key;
+in {
   config = {
     environment.systemPackages = [
       pkgs.sops
       pkgs.age
     ];
 
-    # Embed the bootstrap age key into the image
-    environment.etc."sops/age.key".source = ../secrets/age.key;
+    # Embed the bootstrap age key into the image (works during image build)
+    environment.etc."sops/age.key".source =
+      pkgs.runCommand "bootstrap-age-key"
+        { }
+        ''
+          mkdir -p $out
+          echo '${bootstrapAgeKey}' > $out/age.key
+        '';
 
     sops = {
       defaultSopsFile = ../secrets/secrets.sops.yaml;
 
       age = {
-        # Use both the bootstrap key and the SSH host key
         keyFile = "/etc/sops/age.key";
         sshKeyPaths = [
           "/etc/ssh/ssh_host_ed25519_key"
