e2082f6fc295d32f2492753661b065b7e5bf21e5
Ryan Holt <ryan@ryanholt.net>
2025-04-08 17:27:07 -0400

    show the motd at login

--- modules/shell.nix
+++ modules/shell.nix
@@ -5,7 +5,15 @@
     fish
   ];
 
-  programs.fish.enable = true;
+  programs.fish = {
+    enable = true;
+
+    interactiveShellInit = ''
+      if test -f /etc/motd_age_notice
+        cat /etc/motd_age_notice
+      end
+    '';
+  };
 
   # Optional: Set fish as the default shell system-wide (you already set it per user)
   # users.defaultUserShell = pkgs.fish;
