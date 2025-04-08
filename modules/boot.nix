6df14c9169c1e895515434b17f6700fc15e38d8c
Ryan Holt <ryan@ryanholt.net>
2025-04-07 23:29:57 -0400

    revert back to settings

--- modules/boot.nix
+++ modules/boot.nix
@@ -2,17 +2,14 @@
 {
   boot = {
     loader.generic-extlinux-compatible.enable = true;
-
     kernelModules = [ "dwc2" "g_serial" ];
-    initrd.kernelModules = [ "dwc2" ];
-
     extraModprobeConfig = ''
       options g_serial use_acm=1
     '';
-
     kernelParams = [
       "modules-load=dwc2,g_serial"
       "console=ttyGS0,115200"
     ];
+    initrd.kernelModules = [ "dwc2" ];
   };
 }
