65507edb6f88471c532426e7b4e821e15c2575ab
Ryan Holt <ryan@ryanholt.net>
2025-04-08 14:29:25 -0400

    move to iwd

--- modules/networking.nix
+++ modules/networking.nix
@@ -2,67 +2,27 @@
 
 {
   networking = {
-    networkmanager = {
-      enable = true;
-
-      ensureProfiles = {
-        environmentFiles = [
-          config.sops.secrets.wifiEnv.path
-        ];
-        profiles = {
-          iot = {
-            connection = {
-              id = "iot";
-              type = "wifi";
-              interface-name = "wlan0";
-              permissions = "user:root:";
-              autoconnect = true;
-              uuid = "696c69a4-f0da-4be1-a773-40363d841364";
-            };
-            wifi = {
-              mode = "infrastructure";
-              ssid = "$IOT_WIFI_SSID";
-            };
-            wifi-security = {
-              auth-alg = "open";
-              key-mgmt = "wpa-psk";
-              psk = "$IOT_WIFI_PASSWORD";
-            };
-            ipv4.method = "auto";
-            ipv6 = {
-              addr-gen-mode = "stable-privacy";
-              method = "auto";
-            };
-          };
-
-          rvproblems = {
-            connection = {
-              id = "rvproblems-2ghz";
-              type = "wifi";
-              interface-name = "wlan0";
-              permissions = "user:root:";
-              autoconnect = true;
-              uuid = "e356ddcb-e83c-4e28-af65-3fce873284a6";
-            };
-            wifi = {
-              mode = "infrastructure";
-              ssid = "$RVPROBLEMS_WIFI_SSID";
-            };
-            wifi-security = {
-              auth-alg = "open";
-              key-mgmt = "wpa-psk";
-              psk = "$RVPROBLEMS_WIFI_PASSWORD";
-            };
-            ipv4.method = "auto";
-            ipv6 = {
-              addr-gen-mode = "stable-privacy";
-              method = "auto";
-            };
-          };
-        };
-      };
-    };
-
+    networkmanager.enable = false;
+    wireless.iwd.enable = true;
     hostName = "nixpi";
+  };
+
+  # Declare iwd network profiles using environment.etc
+  environment.etc = {
+    "iwd/iot.psk".text = ''
+      [Security]
+      PreSharedKey=${config.sops.placeholder.IOT_WIFI_PASSWORD}
+
+      [Settings]
+      AutoConnect=true
+    '';
+
+    "iwd/rvproblems-2ghz.psk".text = ''
+      [Security]
+      PreSharedKey=${config.sops.placeholder.RVPROBLEMS_WIFI_PASSWORD}
+
+      [Settings]
+      AutoConnect=true
+    '';
   };
 }
