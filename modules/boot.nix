449b81733b65ae2393c74590500694f7be3633ce
Ryan Holt <ryan@ryanholt.net>
2025-04-07 23:24:50 -0400

    use the fimrwareConfig section instead

--- modules/boot.nix
+++ modules/boot.nix
@@ -14,19 +14,9 @@
       "modules-load=dwc2,g_serial"
       "console=ttyGS0,115200"
     ];
-  };
 
-  # 👇 Move this outside of `boot`
-  system.extraSystemBuilderCmds = ''
-    mkdir -p $out/firmware
-    echo "[pi4]" > $out/firmware/config.txt
-    echo "kernel=u-boot-rpi4.bin" >> $out/firmware/config.txt
-    echo "enable_gic=1" >> $out/firmware/config.txt
-    echo "armstub=armstub8-gic.bin" >> $out/firmware/config.txt
-    echo "" >> $out/firmware/config.txt
-    echo "[all]" >> $out/firmware/config.txt
-    echo "arm_64bit=1" >> $out/firmware/config.txt
-    echo "enable_uart=1" >> $out/firmware/config.txt
-    echo "dtoverlay=dwc2" >> $out/firmware/config.txt
-  '';
+    loader.raspberryPi.firmwareConfig = ''
+      dtoverlay=dwc2
+    '';
+  };
 }
