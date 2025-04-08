8d92110cc2f570d1de7c5ea9b63c3c82a6f830bc
Ryan Holt <ryan@ryanholt.net>
2025-04-07 23:23:39 -0400

    nesting is wrong

--- modules/boot.nix
+++ modules/boot.nix
@@ -14,19 +14,19 @@
       "modules-load=dwc2,g_serial"
       "console=ttyGS0,115200"
     ];
-
-    # Ensures /boot/config.txt gets the dtoverlay=dwc2 line
-    extraSystemBuilderCmds = ''
-      mkdir -p $out/firmware
-      echo "[pi4]" > $out/firmware/config.txt
-      echo "kernel=u-boot-rpi4.bin" >> $out/firmware/config.txt
-      echo "enable_gic=1" >> $out/firmware/config.txt
-      echo "armstub=armstub8-gic.bin" >> $out/firmware/config.txt
-      echo "" >> $out/firmware/config.txt
-      echo "[all]" >> $out/firmware/config.txt
-      echo "arm_64bit=1" >> $out/firmware/config.txt
-      echo "enable_uart=1" >> $out/firmware/config.txt
-      echo "dtoverlay=dwc2" >> $out/firmware/config.txt
-    '';
   };
+
+  # 👇 Move this outside of `boot`
+  system.extraSystemBuilderCmds = ''
+    mkdir -p $out/firmware
+    echo "[pi4]" > $out/firmware/config.txt
+    echo "kernel=u-boot-rpi4.bin" >> $out/firmware/config.txt
+    echo "enable_gic=1" >> $out/firmware/config.txt
+    echo "armstub=armstub8-gic.bin" >> $out/firmware/config.txt
+    echo "" >> $out/firmware/config.txt
+    echo "[all]" >> $out/firmware/config.txt
+    echo "arm_64bit=1" >> $out/firmware/config.txt
+    echo "enable_uart=1" >> $out/firmware/config.txt
+    echo "dtoverlay=dwc2" >> $out/firmware/config.txt
+  '';
 }
