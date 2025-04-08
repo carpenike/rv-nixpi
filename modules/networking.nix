f1749a279eea0fbf86508bed5304745058cc6575
Ryan Holt <ryan@ryanholt.net>
2025-04-08 16:34:39 -0400

    update sops key, adjust where the psk files get written to

--- modules/networking.nix
+++ modules/networking.nix
@@ -1,17 +1,15 @@
 { config, pkgs, ... }:
-let
-  _ = builtins.trace "SOPS placeholders available: ${builtins.toJSON (builtins.attrNames config.sops.placeholder)}" null;
 
-in {
+{
   networking = {
     networkmanager.enable = false;
     wireless.iwd.enable = true;
     hostName = "nixpi";
   };
 
-  # Declare iwd network profiles using environment.etc
+  # Just place the decrypted full .psk content at the expected location
   environment.etc = {
-    "iwd/iot.psk".source = config.sops.secrets.IOT_WIFI_PASSWORD.path;
-    "iwd/rvproblems-2ghz.psk".source = config.sops.secrets.RVPROBLEMS_WIFI_PASSWORD.path;
+    "var/lib/iwd/iot.psk".source = config.sops.secrets.IOT_WIFI_PASSWORD.path;
+    "var/lib/iwd/rvproblems-2ghz.psk".source = config.sops.secrets.RVPROBLEMS_WIFI_PASSWORD.path;
   };
 }
