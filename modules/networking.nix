6c7a72b8d007a877009cfa4b1d404d2a0671e972
Ryan Holt <ryan@ryanholt.net>
2025-04-08 17:34:27 -0400

    use content instead of path

--- modules/networking.nix
+++ modules/networking.nix
@@ -7,9 +7,8 @@
     hostName = "nixpi";
   };
 
-  # Use tmpfiles.d to copy the decrypted secrets to the IWD runtime path
-  systemd.tmpfiles.rules = [
-    "f /var/lib/iwd/iot.psk 0600 root root - ${config.sops.secrets.IOT_WIFI_PASSWORD.path}"
-    "f /var/lib/iwd/rvproblems-2ghz.psk 0600 root root - ${config.sops.secrets.RVPROBLEMS_WIFI_PASSWORD.path}"
-  ];
+  environment.etc = {
+    "var/lib/iwd/iot.psk".text = config.sops.secrets.IOT_WIFI_PASSWORD.content;
+    "var/lib/iwd/rvproblems-2ghz.psk".text = config.sops.secrets.RVPROBLEMS_WIFI_PASSWORD.content;
+  };
 }
