54b961d79f15778481c55847fd7b8716d1624597
Ryan Holt <ryan@ryanholt.net>
2025-04-08 21:07:33 -0400

    use settings block instead

--- modules/networking.nix
+++ modules/networking.nix
@@ -5,20 +5,21 @@
     networkmanager.enable = false;
     wireless.iwd.enable = true;
     hostName = "nixpi";
+
+    wireless.iwd.settings = {
+      General = {
+        EnableNetworkConfiguration = true;
+      };
+      DriverQuirks = {
+        DefaultInterface = "wlan0";
+      };
+    };
   };
 
   systemd.tmpfiles.rules = [
     "C /var/lib/iwd/iot.psk 0600 root root - ${config.sops.secrets.IOT_WIFI_PASSWORD.path}"
     "C /var/lib/iwd/rvproblems-2ghz.psk 0600 root root - ${config.sops.secrets.RVPROBLEMS_WIFI_PASSWORD.path}"
   ];
-
-  environment.etc."iwd/main.conf".text = ''
-    [General]
-    EnableNetworkConfiguration=true
-
-    [DriverQuirks]
-    DefaultInterface=wlan0
-  '';
 
   systemd.services.iwd.serviceConfig = {
     Restart = "on-failure";
