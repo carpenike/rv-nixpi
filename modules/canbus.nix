f63e3038080247c201c28665277f7a986f70d37b
Ryan Holt <ryan@ryanholt.net>
2025-04-08 23:51:51 -0400

    add canbus config

--- modules/canbus.nix
+++ modules/canbus.nix
@@ -1,86 +1,92 @@
 { config, pkgs, lib, ... }: {
+  # Required kernel modules
   boot = {
-    kernelModules = [ "mcp251x" "can" "can_raw" "can_dev" "spi_bcm2835" ];
-    extraModulePackages = [ ];
+    kernelModules = [
+      "mcp251x"
+      "can"
+      "can_raw"
+      "can_dev"
+      "spi_bcm2835"
+    ];
   };
 
-  hardware.deviceTree.overlays = [
-    {
-      name = "mcp2515-can0";
-      dtsText = ''
-        /dts-v1/;
-        /plugin/;
+  # Enable SPI interface on Pi
+  hardware.spi.enable = true;
 
-        / {
-          compatible = "brcm,bcm2835";
+  hardware.deviceTree = {
+    enable = true;
 
-          fragment@0 {
-            target = <&spi0>;
-            __overlay__ {
-              can0: mcp2515@0 {
-                compatible = "microchip,mcp2515";
-                reg = <0>; // CE0
-                spi-max-frequency = <10000000>;
-                interrupt-parent = <&gpio>;
-                interrupts = <25 0x2>; // GPIO25, falling edge
-                status = "okay";
+    overlays = [
+      {
+        name = "mcp2515-can0";
+        dtsText = ''
+          /dts-v1/;
+          /plugin/;
+
+          / {
+            compatible = "brcm,bcm2835";
+
+            fragment@0 {
+              target-path = "/soc/spi@7e204000";
+              __overlay__ {
+                can0: mcp2515@0 {
+                  compatible = "microchip,mcp2515";
+                  reg = <0>; // CE0
+                  spi-max-frequency = <10000000>;
+                  interrupt-parent = <&gpio>;
+                  interrupts = <25 0x2>; // falling edge
+                  status = "okay";
+                };
               };
             };
           };
+        '';
+      }
 
-          fragment@1 {
-            target = <&gpio>;
-            __overlay__ {
-              pinctrl-names = "default";
-              pinctrl-0 = <&can0_pins>;
-            };
-          };
+      {
+        name = "mcp2515-can1";
+        dtsText = ''
+          /dts-v1/;
+          /plugin/;
 
-          __overrides__ {
-            spi0 = "/soc/spi@7e204000";
-            gpio = "/soc/gpio@7e200000";
-          };
-        };
-      '';
-    }
+          / {
+            compatible = "brcm,bcm2835";
 
-    {
-      name = "mcp2515-can1";
-      dtsText = ''
-        /dts-v1/;
-        /plugin/;
-
-        / {
-          compatible = "brcm,bcm2835";
-
-          fragment@0 {
-            target = <&spi0>;
-            __overlay__ {
-              can1: mcp2515@1 {
-                compatible = "microchip,mcp2515";
-                reg = <1>; // CE1
-                spi-max-frequency = <10000000>;
-                interrupt-parent = <&gpio>;
-                interrupts = <24 0x2>; // GPIO24, falling edge
-                status = "okay";
+            fragment@0 {
+              target-path = "/soc/spi@7e204000";
+              __overlay__ {
+                can1: mcp2515@1 {
+                  compatible = "microchip,mcp2515";
+                  reg = <1>; // CE1
+                  spi-max-frequency = <10000000>;
+                  interrupt-parent = <&gpio>;
+                  interrupts = <24 0x2>; // falling edge
+                  status = "okay";
+                };
               };
             };
           };
+        '';
+      }
+    ];
+  };
 
-          fragment@1 {
-            target = <&gpio>;
-            __overlay__ {
-              pinctrl-names = "default";
-              pinctrl-0 = <&can1_pins>;
-            };
-          };
+  # Optional: bring up can interfaces at boot with bitrate
+  systemd.services."can0" = {
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.iproute2}/bin/ip link set can0 up type can bitrate 500000";
+      ExecStop = "${pkgs.iproute2}/bin/ip link set can0 down";
+      Restart = "on-failure";
+    };
+  };
 
-          __overrides__ {
-            spi0 = "/soc/spi@7e204000";
-            gpio = "/soc/gpio@7e200000";
-          };
-        };
-      '';
-    }
-  ];
+  systemd.services."can1" = {
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.iproute2}/bin/ip link set can1 up type can bitrate 500000";
+      ExecStop = "${pkgs.iproute2}/bin/ip link set can1 down";
+      Restart = "on-failure";
+    };
+  };
 }
