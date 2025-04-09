f1a79ac7937d8b4151cbbecda6c6f66b267910e9
Ryan Holt <ryan@ryanholt.net>
2025-04-08 21:04:47 -0400

    add configuration defiinitions

--- modules/networking.nix
+++ modules/networking.nix
@@ -11,4 +11,17 @@
     "C /var/lib/iwd/iot.psk 0600 root root - ${config.sops.secrets.IOT_WIFI_PASSWORD.path}"
     "C /var/lib/iwd/rvproblems-2ghz.psk 0600 root root - ${config.sops.secrets.RVPROBLEMS_WIFI_PASSWORD.path}"
   ];
+
+  environment.etc."iwd/main.conf".text = ''
+    [General]
+    EnableNetworkConfiguration=true
+
+    [DriverQuirks]
+    DefaultInterface=wlan0
+  '';
+
+  systemd.services.iwd.serviceConfig = {
+    Restart = "on-failure";
+    RestartSec = 2;
+  };
 }
