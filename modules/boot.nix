fe3cc36cf0972ff9d1f7f10321fbee64e07c25da
Ryan Holt <ryan@ryanholt.net>
2025-04-21 16:08:57 -0400

    try using the rasperry pi thing

--- modules/boot.nix
+++ modules/boot.nix
@@ -36,6 +36,19 @@
     # Use the Raspberry Pi-specific kernel
     kernelPackages = pkgs.linuxPackages_rpi4;
 
+    # Enable Raspberry Pi firmware to set dtparam and dtoverlay entries
+    boot.firmware.raspberryPi.enable = true;
+    boot.firmware.raspberryPi.config = {
+      dtparam = {
+        spi  = "on";
+        sdio = "on";
+      };
+      dtoverlay = [
+        "mcp2515-can0,oscillator=16000000,interrupt=25"
+        "mcp2515-can1,oscillator=16000000,interrupt=24"
+      ];
+    };
+
     # Removed the spidev-fix.patch as it is already applied in the kernel source.
   };
 }
