a968ba4701485707de611a208ecbcce84bf4f74b
Ryan Holt <ryan@ryanholt.net>
2025-04-21 23:21:41 -0400

    add some formating

--- modules/shell.nix
+++ modules/shell.nix
@@ -12,7 +12,19 @@
     function fish_greeting
       set -l system_path (readlink /run/current-system)
       set -l system_label (basename $system_path)
-      echo "Welcome to $(hostname). Kernel: $(uname -r). System: $system_label"
+      set -l hostname (hostname)
+      set -l kernel_version (uname -r)
+
+      # Define colors
+      set -l color_host blue
+      set -l color_kernel green
+      set -l color_system yellow
+      set -l color_reset normal
+
+      # Print formatted greeting
+      printf "Welcome to %s%s%s\n" (set_color $color_host) $hostname (set_color $color_reset)
+      printf "  %-8s %s%s%s\n" "Kernel:" (set_color $color_kernel) $kernel_version (set_color $color_reset)
+      printf "  %-8s %s%s%s\n" "System:" (set_color $color_system) $system_label (set_color $color_reset)
     end
   '';
 
