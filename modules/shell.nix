a5e0e52c3e2cc8581e8e247516d0ad07741556db
Ryan Holt <ryan@ryanholt.net>
2025-04-21 23:19:28 -0400

    add some login shell info

--- modules/shell.nix
+++ modules/shell.nix
@@ -7,6 +7,15 @@
 
   programs.fish.enable = true;
 
+  # Custom greeting message
+  programs.fish.interactiveShellInit = ''
+    function fish_greeting
+      set -l system_path (readlink /run/current-system)
+      set -l system_label (basename $system_path)
+      echo "Welcome to $(hostname). Kernel: $(uname -r). System: $system_label"
+    end
+  '';
+
   # Optional: Set fish as the default shell system-wide (you already set it per user)
   # users.defaultUserShell = pkgs.fish;
 }
