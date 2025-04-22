0391d61ded3b516465e8b75558a65a64fd1d52c7
Ryan Holt <ryan@ryanholt.net>
2025-04-21 23:25:01 -0400

    can't set hostname variable

--- modules/shell.nix
+++ modules/shell.nix
@@ -12,7 +12,7 @@
     function fish_greeting
       set -l system_path (readlink /run/current-system)
       set -l system_label (basename $system_path)
-      set -l hostname (hostname)
+      # set -l host_name (hostname) # Removed intermediate variable
       set -l kernel_version (uname -r)
 
       # Define colors (vibrant for dark backgrounds)
@@ -22,7 +22,7 @@
       set -l color_reset normal
 
       # Print formatted greeting
-      printf "Welcome to %s%s%s\n" (set_color $color_host) $hostname (set_color $color_reset)
+      printf "Welcome to %s%s%s\n" (set_color $color_host) (hostname) (set_color $color_reset) # Use command substitution directly
       printf "  %-8s %s%s%s\n" "Kernel:" (set_color $color_kernel) $kernel_version (set_color $color_reset)
       printf "  %-8s %s%s%s\n" "System:" (set_color $color_system) $system_label (set_color $color_reset)
     end
