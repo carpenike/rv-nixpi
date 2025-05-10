5128f6830bcf69be076b574e9d822c360c17c3f0
Ryan Holt <ryan@ryanholt.net>
2025-05-10 17:41:13 -0400

    create user outside scope of cloudflare

--- modules/cloudflared.nix
+++ modules/cloudflared.nix
@@ -1,4 +1,3 @@
-# modules/cloudflared.nix
 { config, pkgs, lib, ... }:
 
 let
@@ -6,24 +5,20 @@ let
 in {
   options.services.rvcCloudflared = {
     enable = lib.mkEnableOption "Enable the Cloudflare Tunnel daemon";
-
     configFile = lib.mkOption {
       type = lib.types.path;
       default = "/etc/cloudflared/config.yml";
       description = "Path to the cloudflared tunnel configuration file";
     };
-
     credentialsFile = lib.mkOption {
       type = lib.types.path;
       default = config.sops.secrets.cloudflared_tunnel_credentials.path;
       description = "Path to the cloudflared tunnel credentials file (from SOPS)";
     };
-
     hostname = lib.mkOption {
       type = lib.types.str;
       description = "Hostname for ingress (Cloudflare DNS)";
     };
-
     service = lib.mkOption {
       type = lib.types.str;
       default = "http://localhost:8000";
@@ -31,7 +26,8 @@ in {
     };
   };
 
-  config = lib.mkIf config.services.cloudflared.enable {
+  config = {
+    # ensure user is always created so SOPS can write the file
     users.users.${cloudflaredUser} = {
       isSystemUser = true;
       group = cloudflaredUser;
@@ -39,26 +35,29 @@ in {
 
     users.groups.${cloudflaredUser} = {};
 
-    environment.systemPackages = with pkgs; [ cloudflared ];
+    # only install the rest if enabled
+    config = lib.mkIf config.services.rvcCloudflared.enable {
+      environment.systemPackages = with pkgs; [ cloudflared ];
 
-    environment.etc."cloudflared/config.yml".text = ''
-      tunnel: ${builtins.baseNameOf config.services.cloudflared.credentialsFile}
-      credentials-file: ${config.services.cloudflared.credentialsFile}
+      environment.etc."cloudflared/config.yml".text = ''
+        tunnel: ${builtins.baseNameOf config.services.rvcCloudflared.credentialsFile}
+        credentials-file: ${config.services.rvcCloudflared.credentialsFile}
 
-      ingress:
-        - hostname: ${config.services.cloudflared.hostname}
-          service: ${config.services.cloudflared.service}
-        - service: http_status:404
-    '';
+        ingress:
+          - hostname: ${config.services.rvcCloudflared.hostname}
+            service: ${config.services.rvcCloudflared.service}
+          - service: http_status:404
+      '';
 
-    systemd.services.cloudflared = {
-      description = "Cloudflare Tunnel Daemon";
-      after = [ "network.target" ];
-      wantedBy = [ "multi-user.target" ];
-      serviceConfig = {
-        ExecStart = "${pkgs.cloudflared}/bin/cloudflared tunnel --config ${config.services.cloudflared.configFile} run";
-        Restart = "always";
-        User = cloudflaredUser;
+      systemd.services.cloudflared = {
+        description = "Cloudflare Tunnel Daemon";
+        after = [ "network.target" ];
+        wantedBy = [ "multi-user.target" ];
+        serviceConfig = {
+          ExecStart = "${pkgs.cloudflared}/bin/cloudflared tunnel --config ${config.services.rvcCloudflared.configFile} run";
+          Restart = "always";
+          User = cloudflaredUser;
+        };
       };
     };
   };
